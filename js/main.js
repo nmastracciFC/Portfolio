11e8ccdd57cd448db96b2fc34b5148d647db4b7d
nmastracci <mastracci.natalie@gmail.com>
2017-10-07 21:35:26 -0400

    added sass

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@
 	"use strict";
 	console.log("works!");
 
+var initialBox = document.querySelector("#initial"),
+	ypos = window.pageYOffset;
+console.log(initialBox);
+
+function fadeIn(){
+	console.log(ypos);
+	
+		initialBox.classList.add("hidden");
+
+	};
+	
+
+}
+
+
+
+window.addEventListener("scroll", fadeIn, false);
+
 
 
 })();
